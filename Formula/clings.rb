0b90323c8ffe0ced0ac5e106e00e58616b2bedde
Dan Hart <dan-hart@users.noreply.github.com>
2025-12-05 09:16:50 -0600

    Update clings to v0.1.5

--- Formula/clings.rb
+++ Formula/clings.rb
@@ -1,8 +1,8 @@
 class Clings < Formula
   desc "A feature-rich CLI for Things 3 on macOS"
   homepage "https://github.com/dan-hart/clings"
-  url "https://github.com/dan-hart/clings/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "dc56ed57f3774ea036e60e5c623f981ed3442124458efe1a6ec7b51a9ea94023"
+  url "https://github.com/dan-hart/clings/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "03bf46ed115213bd2899cb361dc719a424031f652b97a7bbfeca25b8ec86f09f"
   license "GPL-3.0-only"
 
   depends_on :macos
