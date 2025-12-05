1026c1b30cc2eb6836e29ee17db6d67eb665c491
Dan Hart <dan-hart@users.noreply.github.com>
2025-12-05 09:19:47 -0600

    Update reprise to v0.1.5

--- Formula/reprise.rb
+++ Formula/reprise.rb
@@ -1,8 +1,8 @@
 class Reprise < Formula
   desc "A fast, feature-rich CLI for Bitrise"
   homepage "https://github.com/dan-hart/reprise"
-  url "https://github.com/dan-hart/reprise/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/dan-hart/reprise/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "0ae4e25519427e2f9746bc66a003a6f2d1ec910632789e68a5fe3ca0f92d58af"
   license "GPL-3.0-only"
 
   depends_on "rust" => :build
