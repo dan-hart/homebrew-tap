c8c331e04c272ffd617a111a7b7e28527533bb70
Dan Hart <dan-hart@users.noreply.github.com>
2025-12-10 18:52:08 -0600

    Fix clings v0.2.0 sha256

--- Formula/clings.rb
+++ Formula/clings.rb
@@ -2,7 +2,7 @@ class Clings < Formula
   desc "A feature-rich CLI for Things 3 on macOS"
   homepage "https://github.com/dan-hart/clings"
   url "https://github.com/dan-hart/clings/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "080fa48515ecc832c9822fb05cc5bf43c6715fdb4b96421a39e99cd6beb1a2ce"
+  sha256 "b35812c0607105e13026b6d04f647057c40ec5d652315f48af8baa416a4c4ec8"
   license "GPL-3.0-or-later"
 
   depends_on :macos
