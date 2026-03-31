99ecda32e58751d6d634959a718471a240fe36a3
Dan Hart <dan-hart@users.noreply.github.com>
2026-03-31 14:24:11 -0500

    Fix fav formula audit issues

--- Formula/fav.rb
+++ Formula/fav.rb
@@ -1,10 +1,9 @@
 class Fav < Formula
   desc "Speed-dial for files and folders"
-  homepage "https://github.com/dan-hart/fav"
+  homepage "https://github.com/dan-hart/homebrew-tap"
   url "https://github.com/dan-hart/homebrew-tap/releases/download/fav-v0.2.0/fav-v0.2.0.tar.gz"
   sha256 "c7741af8ca5954f1067b4893dd571dbed81bac367558618de6db0a23a8ef2a64"
   license "GPL-3.0-only"
-  version "0.2.0"
 
   depends_on "rust" => :build
 
