98d1a6ba6af189a62e228f20cc1e0c70925a42aa
Dan Hart <dan-hart@users.noreply.github.com>
2026-03-31 14:22:56 -0500

    Add fav 0.2.0 formula

---
+++ Formula/fav.rb
@@ -0,0 +1,18 @@
+class Fav < Formula
+  desc "Speed-dial for files and folders"
+  homepage "https://github.com/dan-hart/fav"
+  url "https://github.com/dan-hart/homebrew-tap/releases/download/fav-v0.2.0/fav-v0.2.0.tar.gz"
+  sha256 "c7741af8ca5954f1067b4893dd571dbed81bac367558618de6db0a23a8ef2a64"
+  license "GPL-3.0-only"
+  version "0.2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "fav #{version}", shell_output("#{bin}/fav --version")
+  end
+end
